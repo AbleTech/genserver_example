4c44e247b5d11eaafb01a32d8da892689d312091
Nigel Ramsay <nigel.ramsay@abletech.co.nz>
2017-09-22 13:50:14 +1200

    tidy up

--- lib/genserver_example.ex
+++ lib/genserver_example.ex
@@ -1,18 +1,2 @@
 defmodule GenExample do
-  @moduledoc """
-  Documentation for GenExample.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> GenExample.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/genserver_example_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule GenExampleTest do
-  use ExUnit.Case
-  doctest GenExample
-
-  test "greets the world" do
-    assert GenExample.hello() == :world
-  end
-end
